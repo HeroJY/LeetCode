b1307cd0281ba5e6aa02a3ce182ac21042906e20
hejy <hejy@asiainfo.com>
2019-09-02 15:40:21 +0800

    二叉树结点

---
+++ src/main/java/TreeNode.java
@@ -0,0 +1,27 @@
+package main.java;
+
+/**
+ * @program: LeetCode
+ * @description: 二叉树结点定义
+ * @author: hejy
+ * @create: 2019-09-02
+ **/
+public class TreeNode {
+    /**
+     * 结点值
+     */
+    public int val;
+    /**
+     * 左子节点
+     */
+    public TreeNode left;
+    /**
+     * 右子结点
+     */
+    public TreeNode right;
+
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
